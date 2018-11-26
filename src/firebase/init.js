70034c94fcd5cf975c87f419b5f27cc76ee0a0d9
Lucas <lucasfnetto@gmail.com>
2018-11-26 13:21:19 -0300

    config(firebase): adding firebase configuration

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCUhm92gMTFg-aCEUEOMC-RgUgctxDG5EU",
+  authDomain: "disks-catalog-client.firebaseapp.com",
+  databaseURL: "https://disks-catalog-client.firebaseio.com",
+  projectId: "disks-catalog-client",
+  storageBucket: "disks-catalog-client.appspot.com",
+  messagingSenderId: "385641035692"
+};
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebaseApp.firestore()

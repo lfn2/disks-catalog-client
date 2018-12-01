d6e45cdf0b9ef740457c103fa4198b230ad2eea6
Lucas <lucasfnetto@gmail.com>
2018-12-01 16:59:39 -0300

    feat(CollectionCard): adding hover to collection card

--- src/components/collections/CollectionCard.vue
+++ src/components/collections/CollectionCard.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="card">
-    <v-card color="primary">
-      <v-card-title>
-        <h2 class="white--text">{{collection.name}}</h2>
-      </v-card-title>
-    </v-card>
+    <v-hover>
+      <v-card
+        slot-scope="{hover}"
+        :class="`elevation-${hover ? 12 : 2}`"
+        class="mx-auto"
+        color="primary"
+      >
+        <v-card-title>
+          <h2 class="white--text">{{collection.name}}</h2>
+        </v-card-title>
+      </v-card>
+    </v-hover>
   </div>
 </template>
 
